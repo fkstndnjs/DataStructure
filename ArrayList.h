2035b8bbff8e27602e09a9e8064f2364c844e4ef
fkstndnjs <fkstndnjs@naver.com>
2021-09-03 15:03:50 +0900

    2021-09-03

--- ArrayList.h
+++ ArrayList.h
@@ -1,27 +1,31 @@
 #ifndef __ARRAY_LIST_H__
 #define __ARRAY_LIST_H__
 
-#define TRUE 1
-#define FALSE 0
+#define TRUE	1
+#define FALSE	0
 
-#define LIST_LEN 100
+/*** ArrayList의 정의 ****/
+#define LIST_LEN	100
+typedef int LData;
 
-typedef struct
+typedef struct __ArrayList
 {
-	int arr[LIST_LEN];
+	LData arr[LIST_LEN];
 	int numOfData;
 	int curPosition;
 } ArrayList;
 
+
+/*** ArrayList와 관련된 연산들 ****/
 typedef ArrayList List;
 
 void ListInit(List * plist);
-void LInsert(List * plist, int data);
+void LInsert(List * plist, LData data);
 
-int LFirst(List * plist, int * pdata);
-int LNext(List * plist, int * pdata);
+int LFirst(List * plist, LData * pdata);
+int LNext(List * plist, LData * pdata);
 
-int LRemove(List * plist);
+LData LRemove(List * plist);
 int LCount(List * plist);
 
 #endif
